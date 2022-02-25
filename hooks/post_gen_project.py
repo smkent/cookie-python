9a6605452eb5f948664608d7cfbf456d8ffd44aa
Stephen Kent <smkent@smkent.net>
2022-02-24 22:00:22 -0800

    Add initial glue for migrating from pipenv to poetry

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,29 @@
+import subprocess
+
+# Create empty git repository and add generated files
+for call in (
+    ["git", "init", "."],
+    ["git", "add", "."],
+    ["git", "commit", "-m", "Create {{ cookiecutter.project_name }}"],
+):
+    subprocess.check_call(call)
+
+print(
+    """
+{%- if cookiecutter.enable_pypi_publish|lower != 'yes' %}
+To enable Coverage reports:
+- Set the ENABLE_COVERAGE to true in .github/workflows/ci.yml
+
+{% endif -%}
+To enable PyPI publishing:
+- (optional) Set the TEST_PYPI_API_TOKEN env var on your GitHub repository
+{%- if cookiecutter.enable_pypi_publish|lower != 'yes' %}
+- (optional) Set ENABLE_TEST_PYPI_PUBLISH to true in .github/workflows/ci.yml
+{%- endif %}
+- Set the PYPI_API_TOKEN env var on your GitHub repository
+{%- if cookiecutter.enable_pypi_publish|lower != 'yes' %}
+- Set ENABLE_PYPI_PUBLISH to true in .github/workflows/ci.yml
+
+{%- endif %}
+"""
+)
