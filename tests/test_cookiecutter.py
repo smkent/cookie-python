5e0940176ee2da68832983526149cef857e41297
Stephen Kent <smkent@smkent.net>
2022-02-24 19:27:52 -0800

    Toggle PyPI and coverage CI steps with template/env vars

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 from typing import Any
 
+import yaml
+
 
 def test_bake_project(cookies: Any) -> None:
     result = cookies.bake(extra_context=dict(project_name="test-baked-cookie"))
@@ -16,6 +18,25 @@ def test_bake_project(cookies: Any) -> None:
         os.path.join(result.project_path, "test_baked_cookie")
     )
 
+    # Validate CI configuration
+    data = yaml.safe_load(
+        open(
+            os.path.join(
+                result.project_path,
+                ".github",
+                "workflows",
+                "ci.yml",
+            ),
+            "r",
+        )
+    )
+    assert (
+        data["jobs"]["build-test-publish"]["env"]["ENABLE_PYPI_PUBLISH"]
+        is False
+    )
+    assert data["jobs"]["build-test-publish"]["env"]["ENABLE_COVERAGE"] is True
+
+    # Run rendered project's tests
     subprocess.check_call(
         ["make", "update", "all", "dist"], cwd=result.project_path
     )
