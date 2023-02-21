8a56ffe06bc8baade5183014831b3d64b20a69eb
Stephen Kent <smkent@smkent.net>
2023-02-20 22:55:31 -0800

    Implement manage-cookie release action

---
+++ types/semver.pyi
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+class VersionInfo:
+    @staticmethod
+    def parse(version: str) -> VersionInfo:
+        pass
+    def bump_patch(self) -> VersionInfo:
+        pass
